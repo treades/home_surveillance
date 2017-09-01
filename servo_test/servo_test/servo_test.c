5e9ed4aec2ab0d44d118fdf1a6e2216ee2cbac92
Troy Eades <troyeades@gmail.com>
2017-08-31 19:04:24 -0700

    Initial creation of servo_test.c (and really a git test as well)

---
+++ servo_test/servo_test/servo_test.c
@@ -0,0 +1,18 @@
+/*
+ * servo_test.c
+ *
+ * Created: 8/31/2017 6:48:27 PM
+ * Author : troye
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
